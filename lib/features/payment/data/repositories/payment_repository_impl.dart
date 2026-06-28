9057814edaa2bb04d0bcdd6766d7a1d713f5d21d
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-06-29 01:38:00 +0530

    implemented payment repository

--- lib/features/payment/data/repositories/payment_repository_impl.dart
+++ lib/features/payment/data/repositories/payment_repository_impl.dart
@@ -1,28 +1,33 @@
-import 'package:siksha360_task/core/error/failures.dart';
+import 'dart:math';
+import 'package:siksha360_task/features/payment/data/models/payment_transaction_model.dart';
 import 'package:siksha360_task/features/payment/domain/entities/payment_method.dart';
 import 'package:siksha360_task/features/payment/domain/entities/payment_transaction.dart';
 import 'package:siksha360_task/features/payment/domain/repositories/payment_repository.dart';
 
-class ProcessPaymentUseCase {
-  final PaymentRepository repository;
-
-  ProcessPaymentUseCase(this.repository);
-
-  Future<PaymentTransaction> execute({
+class PaymentRepositoryImpl implements PaymentRepository {
+  @override
+  Future<PaymentTransaction> processPayment({
     required String receiverName,
     required String receiverType,
     required double amount,
     required PaymentMethod paymentMethod,
   }) async {
+    await Future.delayed(const Duration(seconds: 5));
+
     try {
-      return await repository.processPayment(
+      final randomNum = Random().nextInt(900000) + 100000;
+      final String transactionID = 'TXN$randomNum';
+
+      return PaymentTransactionModel(
+        transactionId: transactionID,
         receiverName: receiverName,
         receiverType: receiverType,
         amount: amount,
         paymentMethod: paymentMethod,
+        timestamp: DateTime.now(),
       );
     } catch (e) {
-      throw const PaymentFailure();
+      throw Exception('Payment processing failed: $e');
     }
   }
 }
--- lib/features/payment/domain/repositories/payment_repository.dart
+++ lib/features/payment/domain/repositories/payment_repository.dart
@@ -1,5 +1,11 @@
+import 'package:siksha360_task/features/payment/domain/entities/payment_method.dart';
 import 'package:siksha360_task/features/payment/domain/entities/payment_transaction.dart';
 
 abstract class PaymentRepository {
-  Future<PaymentTransaction> processPayment();
+  Future<PaymentTransaction> processPayment({
+    required String receiverName,
+    required String receiverType,
+    required double amount,
+    required PaymentMethod paymentMethod,
+  });
 }
