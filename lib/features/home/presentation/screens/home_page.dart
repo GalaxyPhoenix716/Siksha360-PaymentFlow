8279cb6b0059933ae754d518f7545baf4e8325e8
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-06-29 01:23:31 +0530

    called homeProvider in home page

--- lib/features/home/presentation/screens/home_page.dart
+++ lib/features/home/presentation/screens/home_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:siksha360_task/features/home/presentation/providers/home_provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final homeState = ref.watch(homeProvider);
+
+    return Scaffold(
+      body: homeState.when(
+        data: (user) => const SizedBox(),
+        error: (e, st) => const SizedBox(),
+        loading: () => const SizedBox(),
+      ),
+    );
   }
 }
