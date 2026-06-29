0d73cd81ec36b67c3e8275b33bf192d72c9013ab
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-06-30 00:38:23 +0530

    formatted code

--- lib/features/payment/presentation/screens/payment_page.dart
+++ lib/features/payment/presentation/screens/payment_page.dart
@@ -3,7 +3,7 @@ import 'package:siksha360_task/features/home/domain/entities/student_fee.dart';
 
 class PaymentPage extends StatelessWidget {
   final StudentFee studentFee;
-  
+
   const PaymentPage({super.key, required this.studentFee});
 
   @override
